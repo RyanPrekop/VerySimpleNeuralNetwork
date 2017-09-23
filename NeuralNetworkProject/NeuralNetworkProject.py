404a39ae26fcb9f5ac71aad93e5c15248d583126
callumjones17 <s3601120@student.rmit.edu.au>
2017-09-23 17:14:34 +1000

    Began Example

---
+++ NeuralNetworkProject/NeuralNetworkProject.py
@@ -0,0 +1,9 @@
+x1 = [2, 4, 5, 8, 9] # Number of Hours Slept.
+x2 = [4, 3, 8, 2, 3] # Number of Hours Studied
+
+data = [0,0]
+
+for i in range(2):
+    data[0] = x1[i]
+    data[1] = x2[i]
+    print(data)
