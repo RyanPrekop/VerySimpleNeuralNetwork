b1a7949ff853421afeb01342b194e2dd81d8e9cd
callumjones17 <s3601120@student.rmit.edu.au>
2017-09-24 10:27:44 +1000

    Imported Numpy

--- NeuralNetworkProject/NeuralNetworkProject.py
+++ NeuralNetworkProject/NeuralNetworkProject.py
@@ -1,13 +1,15 @@
-﻿x1 = [1,2,4,5] #Voltages
-x2 = [7,10,12,13] #Currents
+﻿import numpy
 
-result = [1,0.7,0.4,0.2] #Resistances
+x1 = numpy.array([1,2,4,5]) #Voltages
+x2 = numpy.array([7,10,12,13]) #Currents
 
-w = [0.5,0.5] # Weights
+result = numpy.array([1,0.7,0.4,0.2]) #Resistances
 
-data = [0,0]
+w = numpy.array([0.5,0.5]) # Weights
 
-for i in range(2):
+data = numpy.array([0,0])
+
+for i in range(len(x1)):
     data[0] = x1[i]
     data[1] = x2[i]
     print(data)
