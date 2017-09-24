2b186c0d085231f2216c215e3d6d163f536e3f89
callumjones17 <s3601120@student.rmit.edu.au>
2017-09-24 16:11:06 +1000

    Broken

--- NeuralNetworkProject/NeuralNetworkProject.py
+++ NeuralNetworkProject/NeuralNetworkProject.py
@@ -1,6 +1,6 @@
 ﻿import numpy
 
-x1 = numpy.array([1,2,4,5]) #Voltages
+x1 = numpy.array([1,2,3,4]) #Voltages
 x2 = numpy.array([7,10,12,13]) #Currents
 MAX_CURRENT = 16
 MAX_VOLTAGE = 5
@@ -27,16 +27,19 @@ def run_network_once(y1, y2):
     output[1] =  (float)(y2*w[1])
     return (float)(output[0]+output[1])
 
-print(run_network_once(1,7))
-
 def train_once(d1,d2, r1,w1):
-    for i in range(len(result)):
-        data[0] = d1[i]
-        data[1] = d2[i]
-        w2 = a*(r1[i]-run_network_once(d1[i],d2[i]))*data
-        w1=w1+w2
-    print(w1);
+    #for i in range(len(result)):
+    data[0] = d1[0]
+    data[1] = d2[0]
+    w2 = a*(r1[0]-run_network_once(d1[0],d2[0]))*data
+        #w1=w1+w2
+    w1 = numpy.add(w1,w2)
     return w1
 
+#print(run_network_once(1,7))
+
+
+
 w=train_once=(x1,x2,result,w)
-print(run_network_once(1,7))
+print(w)
+#print(run_network_once(1,7))
